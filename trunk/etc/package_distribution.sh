ec1848666ccd608bf9b63eb962bb780dfc286fc2
api.kurrik@google.com <api.kurrik@google.com@f118d862-acf7-11dd-9a14-c1645b63b0c6>
2009-03-25 21:02:14 +0000

    Updated various scripts

--- trunk/etc/package_distribution.sh
+++ trunk/etc/package_distribution.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 # Script to package up the library for distribution, all pretty like
 
-rm -r dist
-mkdir dist
-svn export src dist/osapi
-svn export examples dist/osapi/examples
-for file in dist/osapi/examples/*.php
-do
-  echo Replacing include path on $file
-  awk '{gsub(/..\/src/, ".."); print}' $file > $file.new
-  rm $file
-  mv $file.new $file
-done
+svn export src opensocial-php-client
+cp LICENSE README NOTICE opensocial-php-client
 
-cp LICENSE README NOTICE dist/osapi
-tar --directory dist -czf dist/osapi.tar.gz osapi
+zip -r osapi-`date "+%Y%m%d"`.zip opensocial-php-client
+tar -cjvf osapi-`date "+%Y%m%d"`.tar.bz opensocial-php-client
+tar  -czf osapi-`date "+%Y%m%d"`.tar.gz opensocial-php-client
 
-
+rm -r opensocial-php-client
