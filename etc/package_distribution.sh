a0dfc34d1a266f0b446ab7b0a53f686dff443336
api.kurrik <api.kurrik@google.com>
2009-03-31 23:26:15 +0000

    Changed packaging script to produce a filename more in line with what we have released in the past.

--- etc/package_distribution.sh
+++ etc/package_distribution.sh
@@ -4,8 +4,8 @@
 svn export src opensocial-php-client
 cp LICENSE README NOTICE opensocial-php-client
 
-zip -r osapi-`date "+%Y%m%d"`.zip opensocial-php-client
-tar -cjvf osapi-`date "+%Y%m%d"`.tar.bz opensocial-php-client
-tar  -czf osapi-`date "+%Y%m%d"`.tar.gz opensocial-php-client
+zip -r opensocial-php-client-`date "+%Y%m%d"`.zip opensocial-php-client
+tar -cjvf opensocial-php-client-`date "+%Y%m%d"`.tar.bz opensocial-php-client
+tar  -czf opensocial-php-client-`date "+%Y%m%d"`.tar.gz opensocial-php-client
 
 rm -r opensocial-php-client
