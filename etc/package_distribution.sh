51aa884032db51de18a96a4c078a1aa30c6b4412
api.kurrik <api.kurrik@google.com>
2009-04-02 18:53:10 +0000

    Changed package script to use a version number instead of date.

--- etc/package_distribution.sh
+++ etc/package_distribution.sh
@@ -2,10 +2,11 @@
 # Script to package up the library for distribution, all pretty like
 
 svn export src opensocial-php-client
-cp LICENSE README NOTICE opensocial-php-client
+cp LICENSE README NOTICE VERSION opensocial-php-client
 
-zip -r opensocial-php-client-`date "+%Y%m%d"`.zip opensocial-php-client
-tar -cjvf opensocial-php-client-`date "+%Y%m%d"`.tar.bz opensocial-php-client
-tar  -czf opensocial-php-client-`date "+%Y%m%d"`.tar.gz opensocial-php-client
+version=`cat VERSION`
+zip -r opensocial-php-client-$version.zip opensocial-php-client
+tar -cjvf opensocial-php-client-$version.tar.bz opensocial-php-client
+tar  -czf opensocial-php-client-$version.tar.gz opensocial-php-client
 
 rm -r opensocial-php-client
