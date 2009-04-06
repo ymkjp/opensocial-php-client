dd995151a71abae5f0d3d74e36e13c1276ba76e0
chabotc <chabotc@f118d862-acf7-11dd-9a14-c1645b63b0c6>
2009-04-06 19:09:54 +0000

    As of today wwww.partuza.nl supports the new method of singing post body's (oauth_body_hash), this patch updates Partuza's provider file to reflect this change (and fixes a small function doc error)

--- src/osapi/providers/osapiPartuzaProvider.php
+++ src/osapi/providers/osapiPartuzaProvider.php
@@ -23,12 +23,28 @@ class osapiPartuzaProvider extends osapiProvider {
   public function __construct(osapiHttpProvider $httpProvider = null) {
     parent::__construct("http://www.partuza.nl/oauth/request_token", "http://www.partuza.nl/oauth/authorize", "http://www.partuza.nl/oauth/access_token", "http://modules.partuza.nl/social/rest", "http://modules.partuza.nl/social/rpc", "Partuza", true, $httpProvider);
   }
+
+  /**
+   * Set's the signer's useBodyHash to true
+   * @param mixed $request The osapiRequest object being processed, or an array
+   *     of osapiRequest objects.
+   * @param string $method The HTTP method used for this request.
+   * @param string $url The url being fetched for this request.
+   * @param array $headers The headers being sent in this request.
+   * @param osapiAuth $signer The signing mechanism used for this request.
+   */
+  public function preRequestProcess(&$request, &$method, &$url, &$headers, osapiAuth &$signer) {
+    if (method_exists($signer, 'setUseBodyHash')) {
+      $signer->setUseBodyHash(true);
+    }
+  }
 }
 
 /**
- * Class for local debugging and development
+ * Class for local debugging and development, inherits the osapiPartuzaProvider's preRequestProcess but overwrites the
+ * constructor to one with a local name: "http://partuza"
  */
-class osapiLocalPartuzaProvider extends osapiProvider {
+class osapiLocalPartuzaProvider extends osapiPartuzaProvider {
   public function __construct(osapiHttpProvider $httpProvider = null) {
     parent::__construct("http://partuza/oauth/request_token", "http://partuza/oauth/authorize", "http://partuza/oauth/access_token", "http://shindig/social/rest", "http://shindig/social/rpc", "LocalPartuza", true, $httpProvider);
   }
