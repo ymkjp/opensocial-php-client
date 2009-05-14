77d839dfa0ca5b2a8921a9997ef1d54b2760aa88
api.kurrik <api.kurrik@google.com>
2009-05-14 13:20:09 +0000

    Instructs iGoogle to use the body hash signing method. Patch by Guy Rutenberg Fixes issue 35

--- src/osapi/providers/osapiGoogleProvider.php
+++ src/osapi/providers/osapiGoogleProvider.php
@@ -28,4 +28,19 @@ class osapiGoogleProvider extends osapiProvider {
   public function __construct(osapiHttpProvider $httpProvider = null) {
     parent::__construct(null, null, null, 'http://www-opensocial-sandbox.googleusercontent.com/api/', 'http://www-opensocial-sandbox.googleusercontent.com/api/rpc', "Google", true, $httpProvider);
   }
+
+  /**
+   * Set's the signer's useBodyHash to true
+   * @param mixed $request The osapiRequest object being processed, or an array
+   *     of osapiRequest objects.
+   * @param string $method The HTTP method used for this request.
+   * @param string $url The url being fetched for this request.
+   * @param array $headers The headers being sent in this request.
+   * @param osapiAuth $signer The signing mechanism used for this request.
+   */
+  public function preRequestProcess(&$request, &$method, &$url, &$headers, osapiAuth &$signer) {
+    if (method_exists($signer, 'setUseBodyHash')) {
+      $signer->setUseBodyHash(true);
+    }
+  }
 }
