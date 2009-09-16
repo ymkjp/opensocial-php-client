8b4fc3b2ba6185ed5da09d8f27f64f32e51a3de4
api.kurrik@google.com <api.kurrik@google.com@f118d862-acf7-11dd-9a14-c1645b63b0c6>
2009-09-16 21:46:45 +0000

    Added a test to fetch @all profile fields.  Added languagesSpoken profile field to orkut tests.

--- trunk/test/online/PersonTest.php
+++ trunk/test/online/PersonTest.php
@@ -41,6 +41,27 @@ class PersonTest extends OnlineTestCase {
     $this->assertEquals($this->suite->USER_A_DISPLAY_NAME, $person->getDisplayName());
   }
   
+  public function testGetAllProfileFields() {
+    $this->assertSupportedMethod('people.get');
+    if ($this->suite->USER_A_EXTENDED_PROFILE_FIELDS == null) {
+      $this->markTestSkipped('Container has no specified extended profile fields.');
+    }
+    
+    $batch = $this->suite->osapi->newBatch();
+    $batch->add($this->suite->osapi->people->get(array('userId' => '@me', 'groupId' => '@self', 'fields' => '@all')), 'self');
+    $result = $batch->execute();
+    $person = $result['self'];
+
+    if ($person instanceof osapiError) {
+      $this->fail($person->getErrorMessage());
+    }
+    
+    $this->assertEquals($this->suite->USER_A_ID, $person->getId());
+    foreach ($this->suite->USER_A_EXTENDED_PROFILE_FIELDS as $field) {
+      $this->assertNotNull($person->getFieldByName($field), "Requested field '$field' should not be null.");
+    }
+  }
+  
   public function testGetExtendedProfileFields() {
     $this->assertSupportedMethod('people.get');
     if ($this->suite->USER_A_EXTENDED_PROFILE_FIELDS == null) {
